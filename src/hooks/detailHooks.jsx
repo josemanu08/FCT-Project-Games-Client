42324e9d21e90300293829d5a1f2f2eb4984314b
josem <josemanuelsanchezvega7@gmail.com>
2024-03-31 15:54:47 +0200

    antes de empezar a dividir los modales

--- src/hooks/detailHooks.jsx
+++ src/hooks/detailHooks.jsx
@@ -27,8 +27,8 @@ export const useXboxTrophies = ({ userId, gameId }) => {
   const [xboxTrophyData, setXboxTrophyData] = useState(null)
   useEffect(() => {
     const fetchData = async () => {
-      const trophies = await getXboxAvailableTrophies(userId, gameId)
-      setXboxTrophyData(trophies)
+      // const trophies = await getXboxAvailableTrophies(userId, gameId)
+      setXboxTrophyData(ALLxboxINFO)
     }
     fetchData()
   }, [])
